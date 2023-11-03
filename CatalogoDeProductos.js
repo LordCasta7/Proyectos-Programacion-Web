804426eb2ca9a8948fb501f2be279e61e417fd51
LaFLSMDFR <miguelhh2423@gmail.com>
2023-11-03 18:28:22 -0500

    Actualizacion

---
+++ CatalogoDeProductos.js
@@ -0,0 +1,15 @@
+const openShopping = document.querySelector(".shopping");
+const closeShopping = document.querySelector(".closeShopping");
+const list = document.querySelector(".list");
+const listCard = document.querySelector(".listCard");
+const body = document.querySelector(".body");
+const total = document.querySelector(".total");
+const quantity = document.querySelector(".quantity");
+
+openShopping.addEventListener("click", () => {
+  body.classList.add("active");
+});
+
+closeShopping.addEventListener("click", () => {
+  body.classList.remove("active");
+});
